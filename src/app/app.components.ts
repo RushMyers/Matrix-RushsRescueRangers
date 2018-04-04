037896c1aeb49d9d035fd541226f694bd307f14a
Rush Myers <myersrush1986@gmail.com>
2018-04-04 15:00:24 -0400

    created header, footer, and animal components and wired them up

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,7 +1,11 @@
 import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AnimalComponent } from './components/animal/animal.component';
 
 export const APP_COMPONENTS = [
     AppComponent,
     HomeComponent,
+    HeaderComponent,
+    AnimalComponent
 ];
