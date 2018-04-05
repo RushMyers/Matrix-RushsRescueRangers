41365e791934454dd7c53a431c9a7afbf3940030
Rush Myers <myersrush1986@gmail.com>
2018-04-04 22:12:30 -0400

    updating linting rules, moved animal attributes to animal component and rendered them dynamically in HTML

--- src/app/components/animal/animal.component.ts
+++ src/app/components/animal/animal.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-animal',
   templateUrl: './animal.component.html',
-  styleUrls: ['./animal.component.css']
+  styleUrls: ['./animal.component.css'],
 })
 export class AnimalComponent implements OnInit {
+  @Input() public name: string;
+  @Input() public species: string;
+  @Input() public imageUrl: string;
 
   constructor() { }
 
