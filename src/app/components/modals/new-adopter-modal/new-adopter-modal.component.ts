716254301b3741689cdd17121ff5ddd3e58c29b4
Rush Myers <myersrush1986@gmail.com>
2018-05-07 12:23:48 -0400

    create clean script and fix bugs with new-adopter-modal component

--- src/app/components/modals/new-adopter-modal/new-adopter-modal.component.ts
+++ src/app/components/modals/new-adopter-modal/new-adopter-modal.component.ts
@@ -2,42 +2,56 @@ import { Component, EventEmitter, OnInit, Input } from '@angular/core';
 
 import { Adopter } from '../../../models/adopter';
 import { Animal } from '../../../models/animal';
-// import { AdopterActions } from '../../../actionHandlers/adopter.actions';
 import { AdoptionActions } from '../../../actionHandlers/adoption.actions';
 import { AppStateActions } from '../../../actionHandlers/appState.actions';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-new-adopter-modal',
-  templateUrl: './new-adopter-modal.component.html',
-  styleUrls: ['./new-adopter-modal.component.css']
+    selector: 'app-new-adopter-modal',
+    templateUrl: './new-adopter-modal.component.html',
+    styleUrls: ['./new-adopter-modal.component.css']
 })
 
 export class NewAdopterModalComponent implements OnInit {
 
-  @Input() animal;
-  public newAdopter: Adopter = new Adopter();
-  public currentAnimal: Animal;
 
-  constructor(
-    // private _adopterActions: AdopterActions,
-    private _adoptionActions: AdoptionActions,
-    private _appStateActions: AppStateActions
-  ) { }
+    @Input() animal;
+    public newAdopter: Adopter = new Adopter();
+    public currentAnimal: Animal;
 
-  // public createAdopter(): void {
-  //   this._adopterActions.createAdopter(this.currentAnimal, this.newAdopter);
-  //   this.closeModal();
-  // }
+    public newAdopterForm: FormGroup;
 
-  public createAdoption(): void {
-    this._adoptionActions.createAdoption({ animal: this.currentAnimal, adopter: this.newAdopter });
-  }
 
-  ngOnInit() {
-    this.currentAnimal = this.animal;
-  }
+    constructor(
+        private _adoptionActions: AdoptionActions,
+        private _appStateActions: AppStateActions,
+        private _fb: FormBuilder
+    ) { }
 
-  public closeModal(): void {
-    this._appStateActions.updateState({ 'modal.isNewAdopterModalShown': false });
-  }
+
+    public createAdoption(): void {
+        this._adoptionActions.createAdoption({ animal: this.currentAnimal, adopter: this.newAdopter });
+    }
+
+    ngOnInit() {
+        this.setUpNewAdopterForm();
+        this.currentAnimal = this.animal;
+    }
+
+
+    private setUpNewAdopterForm(): void {
+        this.newAdopterForm = this._fb.group({
+            firstName: this.newAdopter.firstName,
+            lastName: this.newAdopter.lastName,
+            address: this.newAdopter.address,
+            city: this.newAdopter.address,
+            state: this.newAdopter.state,
+            zipcode: this.newAdopter.zipcode,
+            phoneNo: this.newAdopter.phoneNo
+        });
+    }
+
+    public closeModal(): void {
+        this._appStateActions.updateState({ 'modal.isNewAdopterModalShown': false });
+    }
 }
